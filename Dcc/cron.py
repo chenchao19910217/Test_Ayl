6943f1a17593dcf8035511ddb3384bea82b93d4e
chenchao19910217 <122903166@qq.com>
2020-07-24 21:53:30 +0800

    改bug

--- Dcc/cron.py
+++ Dcc/cron.py
@@ -6,7 +6,7 @@ def test():
     Subject = '来自www.douchacha.com的测试邮件'
     content = 'BUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUGBUG'
     sender = '122903166@qq.com'
-    receiver = ['diao.guanguan@aiyingli.com', '642229662@qq.com']
+    receiver = ['diao.guanguan@aiyingli.com', '642229662@qq.com','chao.chen@aiyingli.com']
     mail.send_mail(Subject, content, sender, receiver)
 
 if __name__ == "__main__":
